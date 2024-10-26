7ccf915522ba2265cff333379c81fdc4452e41a9
jasonmccauley <jsonmccauley@gmail.com>
2024-10-25 22:06:33 -0400

    completed HW05 -- Static Code Analysis

--- HW01 -- Testing Triangle Classification/classify_triangle.py
+++ HW01 -- Testing Triangle Classification/classify_triangle.py
@@ -1,37 +1,31 @@
 import math
 
 def classify_triangle(side1, side2, side3):
-    try:
-        check_inputs(side1, side2, side3)
-        right = check_right(side1, side2, side3)
+    """ main function to classify the triangle according to the inputted sidelengths """
+    check = check_inputs(side1, side2, side3)
+    if isinstance(check, str):
+        return check
+    right = check_right(side1, side2, side3)
 
-        if (side1 == side2 == side3):
-            return "The triangle is equilateral"
-        elif ((side1 == side2) or (side2 == side3) or (side1 == side3)):
-            if (right):
-                return "The triangle is isosceles and right"
-            else:
-                return "The triangle is isosceles"
-        else:
-            if (right):
-                return "The triangle is scalene and right"
-            else:
-                return "The triangle is scalene"
-    except(ValueError, TypeError) as e:
-        return e
+    if side1 == side2 == side3:
+        return "The triangle is equilateral"
+    if (side1 == side2) or (side2 == side3) or (side1 == side3):
+        if right:
+            return "The triangle is isosceles and right"
+        return "The triangle is isosceles"
+    if right:
+        return "The triangle is scalene and right"
+    return "The triangle is scalene"
 
 def check_inputs(num1, num2, num3):
-    if (not isinstance(num1, (int, float)) or not isinstance(num2, (int, float)) or not isinstance(num3, (int, float))):
-        raise TypeError("Please enter numbers for triangle side lengths")
-    
-    if (num1 < 0 or num2 < 0 or num3 < 0):
-        raise ValueError("Triangle side lengths cannot be negative")
-    
-    if ((num1 + num2 < num3) or (num2 + num3 < num1) or (num1 + num3 < num2)):
-        raise ValueError("The sum of two side lengths of a triangle must be greater than the third side length")
+    """ helper function to check if inputted side lengths are proper type, non-negative, and form a valid triangle """
+    if not isinstance(num1, (int, float)) or not isinstance(num2, (int, float)) or not isinstance(num3, (int, float)):
+        return "Please enter numbers for triangle side lengths"
+    if num1 < 0 or num2 < 0 or num3 < 0:
+        return "Triangle side lengths cannot be negative"
+    if (num1 + num2 < num3) or (num2 + num3 < num1) or (num1 + num3 < num2):
+        return "The sum of two side lengths of a triangle must be greater than the third side length"
     
 def check_right(num1, num2, num3):
-    if ((round(math.pow(num1, 2)) + round(math.pow(num2, 2)) == round(math.pow(num3, 2))) or (round(math.pow(num2, 2)) + round(math.pow(num3, 2)) == round(math.pow(num1, 2))) or (round(math.pow(num1, 2)) + round(math.pow(num3, 2)) == round(math.pow(num2, 2)))):
-        return True
-    else:
-        return False
+    """ helper function for checking if the inputted triangle is right """
+    return ((round(math.pow(num1, 2)) + round(math.pow(num2, 2)) == round(math.pow(num3, 2))) or (round(math.pow(num2, 2)) + round(math.pow(num3, 2)) == round(math.pow(num1, 2))) or (round(math.pow(num1, 2)) + round(math.pow(num3, 2)) == round(math.pow(num2, 2))))
--- HW01 -- Testing Triangle Classification/test_classify_triangle.py
+++ HW01 -- Testing Triangle Classification/test_classify_triangle.py
@@ -1,16 +1,52 @@
-from classify_triangle import classify_triangle
-from screen_brand import my_brand
-import pytest
+import unittest
 import math
+from classify_triangle import classify_triangle
 
-my_brand("HW01 -- Testing Triangle Classification")
+class TestClassifyTriangle(unittest.TestCase):
+    """ test suite for the classify_triangle program """
+    def test_equilateral(self):
+        """ test for equilateral triangle, all sides equal """
+        result = classify_triangle(3, 3, 3)
+        self.assertEqual(result, "The triangle is equilateral")
+    
+    def test_isosceles_right(self):
+        """ test for isosceles right triangle, two sides equal and right angle """
+        result = classify_triangle(1, 1, math.sqrt(2))
+        self.assertEqual(result, "The triangle is isosceles and right")
+    
+    def test_isosceles(self):
+        """ test for isosceles triangle, two sides equal only """
+        result1 = classify_triangle(5, 5, 8)
+        result2 = classify_triangle(5, 8 ,5)
+        result3 = classify_triangle(8, 5, 5)
+        self.assertEqual(result1, "The triangle is isosceles")
+        self.assertEqual(result2, "The triangle is isosceles")
+        self.assertEqual(result3, "The triangle is isosceles")
 
-def test_classify_triangle():
-    assert classify_triangle(3, 3, 3) == "The triangle is equilateral"
-    assert classify_triangle(1, 1, math.sqrt(2)) == "The triangle is isosceles and right"
-    assert classify_triangle(5, 5, 8) == "The triangle is isosceles"
-    assert classify_triangle(5, 8 ,5) == "The triangle is isosceles"
-    assert classify_triangle(8, 5, 5) == "The triangle is isosceles"
-    assert classify_triangle(3, 4, 5) == "The triangle is scalene and right"
-    assert classify_triangle(5, 7, 9) == "The triangle is scalene"
-    assert print(classify_triangle(3, 3, "string")) == "Please enter numbers for triangle side lengths"
+    def test_scalene_right(self):
+        """ test for scaelene right triangle, all sides are different and right angle """
+        result = classify_triangle(3, 4, 5)
+        self.assertEqual(result, "The triangle is scalene and right")
+
+    def test_scalene(self):
+        """ test for scalene triangle, all sides different only """
+        result = classify_triangle(5, 7, 9)
+        self.assertEqual(result, "The triangle is scalene")
+
+    def test_check_inputs_type(self):
+        """ test for invalid input with string """
+        result = classify_triangle(3, 4, "string")
+        self.assertEqual(result, "Please enter numbers for triangle side lengths")
+
+    def test_check_inputs_negative(self):
+        """ test for invalid input with negative side lengths """
+        result = classify_triangle(-3, 4, 5)
+        self.assertEqual(result, "Triangle side lengths cannot be negative")
+
+    def test_check_inputs_side_lengths(self):
+        """ test for invalid input that does not form a possible triangle """
+        result = classify_triangle(4, 4, 27)
+        self.assertEqual(result, "The sum of two side lengths of a triangle must be greater than the third side length")
+
+if __name__ == "__main__":
+    unittest.main()
